c2fd1e8980a29c6fa864ca3256b88089eb8dd7c2
edCoder1 <edwin.lsb1@gmail.com>
2020-05-23 19:57:06 -0500

    Thymeleaf v0.2 - Added model attributes on ViewController for template parsing in html files

--- src/main/java/com/example/booking/ViewController.java
+++ src/main/java/com/example/booking/ViewController.java
@@ -1,13 +1,19 @@
 package com.example.booking;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
 
 @Controller
 public class ViewController {
 
     @RequestMapping(value = "/")
-    private String home() {
+    private String home(Model model) {
+        model.addAttribute("username", "edCoder1");
+        model.addAttribute("datetime", new Date());
+        model.addAttribute("mode", "development");
         return "index" ;
     }
 }
