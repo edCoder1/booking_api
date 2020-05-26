b13883092512ea645be405c435a822d6c6a3fe23
edCoder1 <edwin.lsb1@gmail.com>
2020-05-25 21:39:23 -0500

    Basic Configuration from application.properties

--- src/main/java/com/example/booking/ViewController.java
+++ src/main/java/com/example/booking/ViewController.java
@@ -1,5 +1,6 @@
 package com.example.booking;
 
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,11 +10,19 @@ import java.util.Date;
 @Controller
 public class ViewController {
 
+    private String appMode;
+
+
+    // Using dependency injection to inject environment property from configuration
+    public ViewController(Environment env) {
+        appMode = env.getProperty("app-mode");
+    }
+
     @RequestMapping(value = "/")
     private String home(Model model) {
         model.addAttribute("username", "edCoder1");
         model.addAttribute("datetime", new Date());
-        model.addAttribute("mode", "development");
+        model.addAttribute("mode", appMode);
         return "index" ;
     }
 }
